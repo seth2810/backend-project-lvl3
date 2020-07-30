de8caac5feea693521c60cdba28722513d6098d6
Roman Gafurov <gafurov.roma89@gmail.com>
2020-07-31 02:45:03 +0300

    step2: resources download

--- __tests__/page-loader.spec.js
+++ __tests__/page-loader.spec.js
@@ -10,23 +10,48 @@ import helpers from './helpers';
 
 describe('page-loader', () => {
   let outputDir;
+  const pageFileName = 'ru-hexlet-io';
+  const pageUrl = 'https://ru.hexlet.io';
+  const pageFixturePath = helpers.resolveFixturePath('basic.html');
+  const resourcesEntries = Object.entries({
+    '/assets/image.jpg': helpers.resolveFixturePath('assets/image.jpg'),
+    '/assets/script.js': helpers.resolveFixturePath('assets/script.js'),
+    '/assets/style.css': helpers.resolveFixturePath('assets/style.css'),
+  });
 
   beforeEach(async () => {
+    nock(pageUrl).get('/').replyWithFile(200, pageFixturePath);
+
+    resourcesEntries.forEach(([resourcePath, resourceFile]) => {
+      nock(pageUrl).get(resourcePath).replyWithFile(200, resourceFile);
+    });
+
     outputDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader'));
   });
 
   describe('basic', () => {
-    const pageUrl = 'https://hexlet.io';
-    const pageFilename = 'hexlet-io.html';
+    it('should download page file', async () => {
+      await downloadPage(pageUrl, outputDir);
 
-    it('should create page file', async () => {
-      const expected = await fs.readFile(helpers.resolveFixturePath('basic.html'), 'utf-8');
+      const actual = await fs.readFile(path.join(outputDir, `${pageFileName}.html`), 'utf-8');
 
-      nock(pageUrl).get('/').reply(200, expected);
+      expect(actual).toMatchSnapshot();
+    });
+
+    it('should download page resources', async () => {
+      const expected = await Promise.all(
+        resourcesEntries.map(
+          ([, resourceFile]) => fs.readFile(resourceFile, 'utf-8'),
+        ),
+      );
 
       await downloadPage(pageUrl, outputDir);
 
-      const actual = await fs.readFile(path.join(outputDir, pageFilename), 'utf-8');
+      const actual = await Promise.all(
+        resourcesEntries.map(
+          ([resourcePath]) => fs.readFile(path.join(outputDir, `${pageFileName}_files`, resourcePath.slice(1).replace('/', '-')), 'utf-8'),
+        ),
+      );
 
       expect(actual).toEqual(expected);
     });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,118 @@
 import path from 'path';
+import { URL } from 'url';
 import axios from 'axios';
+import cheerio from 'cheerio';
 import { promises as fs } from 'fs';
 
-const convertUrlToFilename = (url) => {
-  const address = url.substr(url.indexOf('//') + 2);
+const getUrlAddress = (url) => url.substr(url.indexOf('//') + 2);
 
-  return address.replace(/[^A-Za-z0-9]/g, '-');
+const convertUrlToFileName = (url) => {
+  const words = Array.from(url.matchAll(/[A-Za-z0-9]+/g));
+
+  return words.join('-');
 };
 
-const downloadPage = (pageUrl, outputDir) => {
-  const fileName = convertUrlToFilename(pageUrl);
-  const outputPath = path.join(outputDir, `${fileName}.html`);
+const convertResourceUrlToFileName = (url) => {
+  const extension = path.extname(url);
+  const urlWithoutExtension = url.substr(0, url.length - extension.length);
 
-  return axios.get(pageUrl)
-    .then(({ data }) => data)
-    .then((data) => fs.appendFile(outputPath, data));
+  return `${convertUrlToFileName(urlWithoutExtension)}${extension}`;
+};
+
+const getResourceUrlAttrubute = (tag) => {
+  switch (tag) {
+    case 'link':
+      return 'href';
+    case 'img':
+    case 'script':
+      return 'src';
+    default:
+      throw new Error(`Unable to define path attribute for tag '${tag}'`);
+  }
+};
+
+const requestUrl = (url) => {
+  const request = axios.get(url, { responseType: 'arraybuffer' });
+
+  return request.then(({ data }) => data);
+};
+
+const extractResourcesUrls = (html) => {
+  const $ = cheerio.load(html);
+  const elements = $('link[rel],script[src],img[src]').toArray();
+
+  return elements.map((element) => {
+    const { tagName } = element;
+    const attribute = getResourceUrlAttrubute(tagName);
+
+    return $(element).attr(attribute);
+  });
+};
+
+const isRelativeResourceUrl = (url, baseUrl) => {
+  const urlWithBase = new URL(url, baseUrl);
+
+  if (urlWithBase.href === url) {
+    return false;
+  }
+
+  return path.extname(url) !== '';
+};
+
+const modifyResourcesPaths = (html, resourcesUrls, resourcesPath) => {
+  const $ = cheerio.load(html);
+
+  $('link[rel],script[src],img[src]').each((index, element) => {
+    const { tagName } = element;
+    const attribute = getResourceUrlAttrubute(tagName);
+    const resourceUrl = $(element).attr(attribute);
+
+    if (!resourcesUrls.includes(resourceUrl)) {
+      return;
+    }
+
+    const resourceFilePath = path.join(resourcesPath, convertResourceUrlToFileName(resourceUrl));
+
+    $(element).attr(attribute, resourceFilePath);
+  });
+
+  return $.html();
+};
+
+const downloadFile = (url, outputPath) => requestUrl(url)
+  .then((data) => fs.appendFile(outputPath, data));
+
+const downloadResources = (resourcesPaths, baseUrl, outputDir) => {
+  const promises = resourcesPaths.map((resourcePath) => {
+    const resourceUrl = new URL(resourcePath, baseUrl);
+    const resourceFilePath = path.join(outputDir, convertResourceUrlToFileName(resourcePath));
+
+    return downloadFile(resourceUrl.href, resourceFilePath);
+  });
+
+  return Promise.all(promises);
+};
+
+const downloadPage = (pageUrl, outputPath) => {
+  const pageFileName = convertUrlToFileName(getUrlAddress(pageUrl));
+
+  const resourcesDir = `${pageFileName}_files`;
+  const resourcesDirPath = path.join(outputPath, resourcesDir);
+  const pageFilePath = path.join(outputPath, `${pageFileName}.html`);
+
+  return requestUrl(pageUrl)
+    .then((data) => data.toString('utf-8'))
+    .then((html) => {
+      const resourcesUrls = extractResourcesUrls(html);
+      const relativeResourcesUrls = resourcesUrls.filter(
+        (value) => isRelativeResourceUrl(value, pageUrl),
+      );
+      const modifiedHtml = modifyResourcesPaths(html, relativeResourcesUrls, resourcesDir);
+
+      return fs.appendFile(pageFilePath, modifiedHtml)
+        .then(() => fs.mkdir(resourcesDirPath))
+        .then(() => downloadResources(relativeResourcesUrls, pageUrl, resourcesDirPath));
+    });
 };
 
 export default downloadPage;
