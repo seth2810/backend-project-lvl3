2bd51015a89c9edf582e7b70145765a1260f026f
Roman Gafurov <gafurov.roma89@gmail.com>
2020-11-07 21:36:46 +0300

    reactoring of page loader flow

--- index.js
+++ index.js
@@ -13,133 +13,105 @@ const log = debug('page-loader');
 
 axiosDebugLog.addLogger(client, log);
 
-const getUrlAddress = (url) => url.substr(url.indexOf('//') + 2);
+const resourcePathAttributes = {
+  img: 'src',
+  link: 'href',
+  script: 'src',
+};
 
-const convertUrlToFileName = (url) => {
-  const words = Array.from(url.matchAll(/[A-Za-z0-9]+/g));
+const slugifyUrl = (url) => {
+  const { hostname, pathname } = url;
+  const words = `${hostname}${pathname}`.match(/\w+/gi);
 
   return words.join('-');
 };
 
-const convertResourceUrlToFileName = (url) => {
-  const extension = path.extname(url);
-  const urlWithoutExtension = url.substr(0, url.length - extension.length);
+const slugifyResourceUrl = (url) => {
+  const { dir, name, ext } = path.parse(url.pathname);
 
-  return `${convertUrlToFileName(urlWithoutExtension)}${extension}`;
+  const fileExtension = ext || '.html';
+  const fileNamePath = path.join(dir, name);
+  const fileNameSlug = slugifyUrl(new URL(fileNamePath, url.origin));
+
+  return `${fileNameSlug}${fileExtension}`;
 };
 
-const getResourceUrlAttrubute = (tag) => {
-  switch (tag) {
-    case 'link':
-      return 'href';
-    case 'img':
-    case 'script':
-      return 'src';
-    default:
-      throw new Error(`Unable to define path attribute for tag '${tag}'`);
-  }
-};
+const extractResources = (html, originUrl, resourcesDirName) => {
+  const $ = cheerio.load(html, { decodeEntities: false });
+  const resources = [];
 
-const requestUrl = (url) => {
-  const request = client.get(url, { responseType: 'arraybuffer' });
+  Object.entries(resourcePathAttributes).forEach(([tag, attribute]) => {
+    const relativeResources = $(`${tag}:not([${attribute}=""])`)
+      .toArray()
+      .map((element) => $(element))
+      .map(($element) => ({ $element, url: new URL($element.attr(attribute), originUrl) }))
+      .filter(({ url }) => url.origin === originUrl);
 
-  return request.then(({ data }) => data).catch((error) => {
-    throw new Error(`${error.message} (${url})`);
-  });
-};
+    relativeResources.forEach(({ $element, url }) => {
+      const fileUrl = url.toString();
+      const filePath = path.join(resourcesDirName, slugifyResourceUrl(url));
 
-const extractResourcesUrls = (html) => {
-  const $ = cheerio.load(html);
-  const elements = $('link[rel],script[src],img[src]').toArray();
+      resources.push({ fileUrl, filePath });
 
-  return elements.map((element) => {
-    const { tagName } = element;
-    const attribute = getResourceUrlAttrubute(tagName);
-
-    return $(element).attr(attribute);
-  });
-};
-
-const isRelativeResourceUrl = (url, baseUrl) => {
-  const urlWithBase = new URL(url, baseUrl);
-
-  if (urlWithBase.href === url) {
-    return false;
-  }
-
-  return path.extname(url) !== '';
-};
-
-const modifyResourcesPaths = (html, resourcesUrls, resourcesPath) => {
-  const $ = cheerio.load(html);
-
-  $('link[rel],script[src],img[src]').each((index, element) => {
-    const { tagName } = element;
-    const attribute = getResourceUrlAttrubute(tagName);
-    const resourceUrl = $(element).attr(attribute);
-
-    if (!resourcesUrls.includes(resourceUrl)) {
-      return;
-    }
-
-    const resourceFilePath = path.join(resourcesPath, convertResourceUrlToFileName(resourceUrl));
-
-    $(element).attr(attribute, resourceFilePath);
+      $element.attr(attribute, filePath);
+    });
   });
 
-  return $.html();
+  const modifiedHtml = $.html();
+
+  return { html: modifiedHtml, resources };
 };
 
-const downloadFile = (url, outputPath) => requestUrl(url)
-  .then((data) => fs.appendFile(outputPath, data));
+const downloadResource = (url, outputPath) => {
+  log('download asset "%s" to "%s"', url, outputPath);
 
-const downloadResources = (resourcesPaths, baseUrl, outputDir) => {
-  const tasks = resourcesPaths.map((resourcePath) => {
-    const resourceUrl = new URL(resourcePath, baseUrl);
-    const resourceFilePath = path.join(outputDir, convertResourceUrlToFileName(resourcePath));
-
-    log('download asset "%s" to "%s"', resourcePath, resourceFilePath);
-
-    return {
-      title: resourceUrl.toString(),
-      task: () => downloadFile(resourceUrl.href, resourceFilePath),
-    };
-  });
-
-  const listr = new Listr(tasks, { concurrent: true, exitOnError: false });
-
-  return listr.run();
+  return client.get(url, { responseType: 'arraybuffer' })
+    .then(({ data }) => fs.writeFile(outputPath, data));
 };
 
-const downloadPage = (pageUrl, outputPath) => {
-  const pageFileName = convertUrlToFileName(getUrlAddress(pageUrl));
-
-  const resourcesDir = `${pageFileName}_files`;
-  const resourcesDirPath = path.join(outputPath, resourcesDir);
+/**
+ 1. prepare url
+ 2. download page html
+ 3. detect assets and modify html
+ 4. save modified html to output dir
+ 5. create assets download tasks
+ 6. wrap tasks with listr
+ */
+const downloadPage = (url, outputPath) => {
+  const pageUrl = new URL(url);
+  const pageFileName = slugifyUrl(pageUrl);
+  const resourcesDirName = `${pageFileName}_files`;
+  const resourcesDirPath = path.join(outputPath, resourcesDirName);
   const pageFilePath = path.join(outputPath, `${pageFileName}.html`);
 
-  log('load page', pageUrl);
-  log('output path', outputPath);
+  log('load', pageUrl);
+  log('output', outputPath);
 
-  return requestUrl(pageUrl)
-    .then((data) => data.toString('utf-8'))
-    .then((html) => {
-      const resourcesUrls = extractResourcesUrls(html);
-      const relativeResourcesUrls = resourcesUrls.filter(
-        (value) => isRelativeResourceUrl(value, pageUrl),
-      );
-      const modifiedHtml = modifyResourcesPaths(html, relativeResourcesUrls, resourcesDir);
+  return client.get(url)
+    .then((response) => {
+      log('create assets directory', resourcesDirPath);
+
+      return fs.access(resourcesDirPath)
+        .catch(() => fs.mkdir(resourcesDirPath))
+        .then(() => response.data);
+    })
+    .then((data) => {
+      const { html, resources } = extractResources(data, pageUrl.origin, resourcesDirName);
 
       log('save page file', pageFilePath);
 
-      return fs.appendFile(pageFilePath, modifiedHtml)
-        .then(() => {
-          log('create assets directory', resourcesDirPath);
+      return fs.appendFile(pageFilePath, html)
+        .then(() => resources);
+    })
+    .then((resources) => {
+      const tasks = resources.map(({ fileUrl, filePath }) => ({
+        title: fileUrl,
+        task: () => downloadResource(fileUrl, path.join(outputPath, filePath)),
+      }));
 
-          return fs.access(resourcesDirPath)
-            .catch(() => fs.mkdir(resourcesDirPath));
-        })
-        .then(() => downloadResources(relativeResourcesUrls, pageUrl, resourcesDirPath));
+      const listr = new Listr(tasks, { concurrent: true, exitOnError: false });
+
+      return listr.run();
     })
     .then(() => pageFilePath);
 };
